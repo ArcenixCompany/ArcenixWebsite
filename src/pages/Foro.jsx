229e6976b5a8bd81bc0a6b4c69dc3432a1ded3dc
LautaroJLZ <lautarojoseluis1234@gmail.com>
2024-02-17 00:30:55 -0300

    add some helps for Nahu

--- src/pages/Foro.jsx
+++ src/pages/Foro.jsx
@@ -1,8 +1,11 @@
 
 const Foro = () => {
     return(
-        <div className="bg-violeta h-screen flex justify-center items-center">Foro</div>
-    )
+        <div className="bg-violeta h-screen flex justify-center items-center">Foro
+        <h2 className="navBar">hola</h2>
+        <h1 className="pepe">Chau</h1>
+        </div>
+            )
 }
 
 export { Foro }
