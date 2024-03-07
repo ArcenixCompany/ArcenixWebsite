07773791031b62a6bdecb995038c1f2a039083f3
LautaroJLZ <lautarojoseluis1234@gmail.com>
2024-03-07 04:31:30 -0300

    Fix auto scroll in home

--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -29,9 +29,9 @@ const Inicio = () => {
     },
   ];
 
-  // useEffect(() => {
-  //   window.scrollTo(0, 0);
-  // }, []);
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
   return (
     <div>
       <div
